112b3d06dcdc86600b2823d4a6102110d6c7ba3b
Ayato Tokubi <atokubi@redhat.com>
2025-08-28 12:00:21 +0000

    Add two sig-node probe tests to retriable tests
    Signed-off-by: Ayato Tokubi <atokubi@redhat.com>

--- pkg/test/ginkgo/cmd_runsuite.go
+++ pkg/test/ginkgo/cmd_runsuite.go
@@ -160,9 +160,11 @@ func shouldRetryTest(ctx context.Context, test *testCase, permittedRetryImageTag
 	// were actually flaky and nobody knew. We attempted to fix these, a few did not make it in time. Restore
 	// retries for specific test names so the overall suite can continue to not retry.
 	retryTestNames := []string{
-		"[sig-instrumentation] Metrics should grab all metrics from kubelet /metrics/resource endpoint [Suite:openshift/conformance/parallel] [Suite:k8s]", // https://issues.redhat.com/browse/OCPBUGS-57477
-		"[sig-network] Services should be rejected for evicted pods (no endpoints exist) [Suite:openshift/conformance/parallel] [Suite:k8s]",               // https://issues.redhat.com/browse/OCPBUGS-57665
-		"[sig-node] Pods Extended Pod Container lifecycle evicted pods should be terminal [Suite:openshift/conformance/parallel] [Suite:k8s]",              // https://issues.redhat.com/browse/OCPBUGS-57658
+		"[sig-instrumentation] Metrics should grab all metrics from kubelet /metrics/resource endpoint [Suite:openshift/conformance/parallel] [Suite:k8s]",                                                     // https://issues.redhat.com/browse/OCPBUGS-57477
+		"[sig-network] Services should be rejected for evicted pods (no endpoints exist) [Suite:openshift/conformance/parallel] [Suite:k8s]",                                                                   // https://issues.redhat.com/browse/OCPBUGS-57665
+		"[sig-node] Pods Extended Pod Container lifecycle evicted pods should be terminal [Suite:openshift/conformance/parallel] [Suite:k8s]",                                                                  // https://issues.redhat.com/browse/OCPBUGS-57658
+		"[sig-node] Probing container should *not* be restarted with a non-local redirect http liveness probe [Suite:openshift/conformance/parallel] [Suite:k8s]",                                              // https://issues.redhat.com/browse/OCPBUGS-59735
+		"[sig-node] [Feature:SidecarContainers] Probing restartable init container should *not* be restarted with a non-local redirect http liveness probe [Suite:openshift/conformance/parallel] [Suite:k8s]", // https://issues.redhat.com/browse/OCPBUGS-60643
 	}
 	for _, rtn := range retryTestNames {
 		if test.name == rtn {
